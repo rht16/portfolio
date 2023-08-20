309736369237a1547becfc02d5c3022af3b6e3f5
Rohit <87421802+rht16@users.noreply.github.com>
2023-08-20 18:55:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import './App.css'
 
 function App() {
 
-  const { theme } = useContext(ThemeContext);
+
 
   // console.log("%cDEVELOPER PORTFOLIO", `color:${theme.primary}; font-size:50px`);
   // console.log("%chttps://github.com/hhhrrrttt222111/developer-portfolio", `color:${theme.tertiary}; font-size:20px`);

fdd7d5cb36b889c5de10762ac60f2e8324773527
Rohit <87421802+rht16@users.noreply.github.com>
2023-08-20 19:00:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
-import { ThemeContext } from './contexts/ThemeContext';
+
 import { Main, BlogPage, ProjectPage } from './pages'
 import { BackToTop } from './components'
 import ScrollToTop from './utils/ScrollToTop'
